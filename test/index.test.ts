c25ca51b699669b7fae99c92c4536596a36f78a2
Fabrizio Zaffarana <fzaffarana@gmail.com>
2022-04-23 04:44:37 -0300

    fix: pre-commit

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import { hello } from '../src/index';
 describe('index', () => {
   describe('.hello', () => {
     it('should say hello to the given name', () => {
-      expect(hello('World')).toBe('Hello World');
+      expect(hello('World')).toBe('Hello World!');
     });
   });
 });
